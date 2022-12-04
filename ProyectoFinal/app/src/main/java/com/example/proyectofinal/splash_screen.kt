76759415913d7194943bec95c1a7997525d7552d
davidcalles1 <69554096+davidcalles1@users.noreply.github.com>
2022-12-04 16:38:48 -0600

    pantalla de carga

---
+++ ProyectoFinal/app/src/main/java/com/example/proyectofinal/splash_screen.kt
@@ -0,0 +1,30 @@
+package com.example.proyectofinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.content.ContextCompat.startActivity
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        starTimer()
+    }
+
+
+    fun starTimer(){
+        object: CountDownTimer(500, 500){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext,MainActivity::class.java).apply {  }
+                startActivity(intent)
+            }
+
+        }.start()
+    }
+}
